0477a2af33a8738670fc90b4926fbfc8993efe9a
theos99 <k21180036@kcl.ac.uk>
2024-04-23 16:32:44 +0100

    fix 0.2

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 // Routes.js
 import React from "react";
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,28 +11,14 @@ const AppRoutes = () => {
   return (
     <Router>
       <Header />
-      <Switch>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/calculate" element={<CalCalculator />} />
-      </Switch>
+      </Routes>
       <Footer />
     </Router>
   );
 };
 
 export default AppRoutes;
-
-
-// function App() {
-//     return (
-//         <Router>
-//             <Switch>
-//                 <Route path="/" exact component={Home} />
-//                 <Route path="/about" component={About} />
-//                 {/* Add more routes here */}
-//             </Switch>
-//         </Router>
-//     );
-// }
-
